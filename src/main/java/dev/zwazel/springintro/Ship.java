5847f8084709f7c464774affb1566eade7c04535
Jonas_Laur <laurj@bzz.ch>
2026-03-04 15:23:12 +0100

    ship basics

--- src/main/java/dev/zwazel/springintro/Ship.java
+++ src/main/java/dev/zwazel/springintro/Ship.java
@@ -9,16 +9,45 @@ import lombok.Setter;
 
 @Getter
 @Setter
-@Entity
+
 public class Ship {
     @Id
     private Long id;
     private Position position;
     private int health;
     private int maxHealth;
-    private int width;
-    private int height;
+    private double width;
+    private double height;
     private long lastShotTime;
     private long shootCooldown;
 
+
+    public moveLeft() {
+        position.setX(position.getX() - 1);
+        return position;
+    }
+    public moveRight() {
+        position.setX(position.getX() + 1);
+        return position;
+    }
+     public boolean canShoot() {
+         return System.currentTimeMillis() - lastShotTime >= shootCooldown;
+     }
+
+     public void shoot() {
+         if (canShoot()) {
+
+             lastShotTime = System.currentTimeMillis();
+         }
+     }
+
+     public void takeDamage(int damage) {
+         health -= damage;
+         if (health < 0) {
+             health = 0;
+         }
+     }
+     public isAlive() {
+         return health > 0;
+     }
 }
